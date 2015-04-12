62002c58f85cd6ea11781d3a3567fc641991d25b
Aitor <aitor.ssnn@gmail.com>
2015-04-12 13:33:44 +0200

    Twitter timeline widget included and other minor changes.

--- mapas3d.php
+++ mapas3d.php
@@ -1,64 +1,75 @@
 <!DOCTYPE HTML>
 <html lang="en">
-  <head>
-    <title>Página proyecto delMasAjax</title>
-    <meta charset="utf-8">
-    <link href="css/globe.css" rel="stylesheet">
-    <meta http-equiv="refresh" content="300">
-  </head>
-  <body>
-	  <div id="container"></div>
-		  <div class="info">
-		    <div class="author">
-				<table>				
-					<tr>				
-						<th></th>				
-						<th>Position(km)</th>				
-						<th>Velocity(m/s)</th>				
-					</tr>				
-					<tr>				
-						<td>X</td>				
-						<td id="USLAB000032"></td>				
-						<td id="USLAB000035"></td>				
-					</tr>				
-					<tr>				
-						<td>Y</td>				
-						<td id="USLAB000033"></td>				
-						<td id="USLAB000036"></td>				
-					</tr>				
-					<tr>				
-						<td>Z</td>				
-						<td id="USLAB000034"></td>				
-						<td id="USLAB000037"></td>				
-					</tr>				
-				</table>		
-		      	<a href="http://github.com/NorthIsUp/orbital2">Orbital Part Deux</a>
-		    </div>
-		    <span class="bull">&bull;</span>
+<head>
+<title>Página proyecto delMasAjax</title>
+<meta charset="utf-8">
+<link href="css/globe.css" rel="stylesheet">
+<meta http-equiv="refresh" content="300">
+</head>
+<body>
+	<div id="container"></div>
+	<div class="rightInfo">
+		<div class="author">
+			<table>
+				<tr>
+					<th></th>
+					<th>Position(km)</th>
+					<th>Velocity(m/s)</th>
+				</tr>
+				<tr>
+					<td>X</td>
+					<td id="USLAB000032"></td>
+					<td id="USLAB000035"></td>
+				</tr>
+				<tr>
+					<td>Y</td>
+					<td id="USLAB000033"></td>
+					<td id="USLAB000036"></td>
+				</tr>
+				<tr>
+					<td>Z</td>
+					<td id="USLAB000034"></td>
+					<td id="USLAB000037"></td>
+				</tr>
+			</table>
+			<a href="http://github.com/NorthIsUp/orbital2">Orbital Part Deux</a>
+		</div>
+		<span class="bull">&bull;</span>
+	</div>
 
-		    <div class="share"><!-- twitter share button -->
-		      <a href="https://twitter.com/share" class="twitter-share-button" data-text="#Python and #Nginx powering Orbital 2.0. Built #PyCon tough." data-via="Disqus" data-related="NorthIsUp">Tweet</a>
-		      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-		    </div>
-		  </div>
-	
-	  <div id="postInfo"></div>
-	
-	  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	  <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.9.0/TweenMax.min.js"></script>
-	  <script src="http://underscorejs.org/underscore-min.js"></script>
-	  <script src="js/third-party/system.min.js"></script>
-	  <script src="js/third-party/three56.js"></script>
-	  <script src="js/third-party/tween.js"></script>
-	  <script src="js/third-party/eventsourcePollyfill.js"></script>
-	  <script src="js/orbital.js"></script>
-	  <script src="js/util.js"></script>
-	  <script src="js/geoutil.js"></script>
-	  <script src="js/point.js"></script>
-	  <script src="js/globe.js"></script>
-	  <script src="js/demo.js"></script>
-  </body>
-	<script>
+	<div class="leftInfo">
+		<a class="twitter-timeline" href="https://twitter.com/ISSData"
+			data-widget-id="587215743219068928">Tweets por el @ISSData.</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+		<div class="share">
+			<!-- twitter share button -->
+			<a href="https://twitter.com/share" class="twitter-share-button"
+				data-text="#Python and #Nginx powering Orbital 2.0. Built #PyCon tough."
+				data-via="Disqus" data-related="NorthIsUp">Tweet</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+	</div>
+
+	<div id="postInfo"></div>
+
+	<script
+		src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script
+		src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.9.0/TweenMax.min.js"></script>
+	<script src="http://underscorejs.org/underscore-min.js"></script>
+	<script src="js/third-party/system.min.js"></script>
+	<script src="js/third-party/three56.js"></script>
+	<script src="js/third-party/tween.js"></script>
+	<script src="js/third-party/eventsourcePollyfill.js"></script>
+	<script src="js/orbital.js"></script>
+	<script src="js/util.js"></script>
+	<script src="js/geoutil.js"></script>
+	<script src="js/point.js"></script>
+	<script src="js/globe.js"></script>
+	<script src="js/demo.js"></script>
+</body>
+<script>
 	      setInterval(function() {
 	      
 	      var xhr;
@@ -77,16 +88,16 @@
 	      xhr.send(null);
 	    },3000);
 	</script>
-	
-	<script src="//demos.lightstreamer.com/commons/require.js"></script>
-	
-	<script src="//demos.lightstreamer.com/commons/lightstreamer.js"></script>
-	
-	<script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
-	
-	
-	
-	<script>
+
+<script src="//demos.lightstreamer.com/commons/require.js"></script>
+
+<script src="//demos.lightstreamer.com/commons/lightstreamer.js"></script>
+
+<script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
+
+
+
+<script>
 	
 	require(["LightstreamerClient","Subscription"],function(LightstreamerClient,Subscription) {
 	
